3c971fc8ca72b0024979e41aa16ffc71f757be0b
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2021-09-21 20:59:22 +0800

    Added react router

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -4,13 +4,13 @@ import Cookies from "universal-cookie";
 const HomePage = () => {
 	const handleClick = () => {
 		const cookies = new Cookies();
-		cookies.set("version", "newApp", { path: "/" });
+		cookies.set("version", "payment", { path: "/" });
 		window.location.reload();
 	};
 	return (
 		<div>
-			<h2>App Version - {process.env.REACT_APP_VERSION}</h2>
-			<button onClick={handleClick}>Navigate to Second App</button>
+			<h2>Home App Version - {process.env.REACT_APP_VERSION}</h2>
+			<button onClick={handleClick}>Navigate to Payment App</button>
 		</div>
 	);
 };
